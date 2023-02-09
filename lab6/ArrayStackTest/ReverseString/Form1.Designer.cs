ee3e70837426f7d8b39342900141e93da57a879b
Aekkasak_Makhamjan <eqadzc@gmail.com>
2023-02-09 11:09:34 +0700

    commit5

---
+++ lab6/ArrayStackTest/ReverseString/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ReverseString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.reverseButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ข้อความต้นฉบับ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ข้อความแบบย้อนกลับ";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(358, 87);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(185, 31);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(358, 148);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(185, 31);
+            this.outputBox.TabIndex = 1;
+            // 
+            // reverseButton
+            // 
+            this.reverseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reverseButton.Location = new System.Drawing.Point(358, 232);
+            this.reverseButton.Name = "reverseButton";
+            this.reverseButton.Size = new System.Drawing.Size(185, 39);
+            this.reverseButton.TabIndex = 2;
+            this.reverseButton.Text = "ปุ่มกลับข้อความ";
+            this.reverseButton.UseVisualStyleBackColor = true;
+            this.reverseButton.Click += new System.EventHandler(this.reverseButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(75, 232);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(185, 39);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "ปุ่มเคลียร์ค่า";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 350);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.reverseButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button reverseButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ lab6/ArrayStackTest/ReverseString/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReverseString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reverseButton_Click(object sender, EventArgs e)
+        {
+            ArrayStack S = new ArrayStack(100);
+            string message = inputBox.Text;
+            for (int i = 0; i < message.Length; ++i)
+            {
+                S.push(message[i]);
+            }
+            string data = "";
+            while (!S.isEmpty())
+            {
+                data = data + S.pop();
+            }
+            outputBox.Text = data;
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+            outputBox.Text = "";
+        }
+    }
+}
